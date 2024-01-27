cc6989e836c8bd3cd6167530986958c49f0935c3
aya-m-98 <106470807+aya-m-98@users.noreply.github.com>
2024-01-27 18:02:05 +0200

    upload

--- Backend/index.js
+++ Backend/index.js
@@ -1,49 +1,54 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-const cors = require('cors');
-dotenv.config()
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cors = require("cors");
+dotenv.config();
 
 //using express
 const app = express();
 //Routs
 
-// const carts = require('./routes/cartsRouts')
-const productsRouts = require('./routes/productsRouts')
-const usersRouts = require('./routes/usersRouts')
-const reviewRouts = require('./routes/reviewsRouts')
-const PaymentRouts = require('./routes/paymentsRouts')
-const wishlistRouts = require('./routes/wishlistRouts')
+const carts = require("./routes/cartsRouts");
+const categories = require("./routes/categoriesRouts");
+const subcategory = require("./routes/subcategoryRouts");
+const coupons = require("./routes/couponsRouts");
+const productsRouts = require("./routes/productsRouts");
+const usersRouts = require("./routes/usersRouts");
+const reviewRouts = require("./routes/reviewsRouts");
+const PaymentRouts = require("./routes/paymentsRouts");
+const wishlistRouts = require("./routes/wishlistRouts");
 //express middleware
 app.use(express.json());
 app.use(cors());
 //routs for All Project routes
-
-// app.use('/Carts', carts)
-app.use('/users', usersRouts)
-app.use('/products', productsRouts)
-app.use('/reviews', reviewRouts)
-app.use('/payment', PaymentRouts)
-app.use('/wishlist', wishlistRouts)
+app.use("/Carts", carts);
+app.use("./Coupons", coupons);
+app.use("/Categories", categories);
+app.use("./subcategory", subcategory);
+app.use("/users", usersRouts);
+app.use("/products", productsRouts);
+app.use("/reviews", reviewRouts);
+app.use("/payment", PaymentRouts);
+app.use("/wishlist", wishlistRouts);
 //mongoose database
-mongoose.connect(process.env.MONGOOSEURL)
-    .then(() => {
-        console.log("Connected to DB");
-    })
-    .catch((err) => {
-        console.error("Error connecting to DB:", err);
-    });
-
+mongoose
+  .connect(process.env.MONGOOSEURL)
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to DB:", err);
+  });
 
 // if path is not specified
 app.use("*", (req, res, next) => {
-    res.status(404).json({ message: "Not Found path" })
-})
+  res.status(404).json({ message: "Not Found path" });
+});
 // error handling
 app.use((err, res, req, next) => {
-    res.status(500).json({ message: "routs not found" })
-})
+  res.status(500).json({ message: "routs not found" });
+});
 //listening
 app.listen(process.env.PORT, () => {
-    console.log(`listening on port ${process.env.PORT}`);
-}) 
+  console.log(`listening on port ${process.env.PORT}`);
+});
