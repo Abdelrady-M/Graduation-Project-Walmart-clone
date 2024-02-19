fb4368d90f390eb633510f6102627ffd12e34889
DevAbdelrady <ahmedmc95@gmail.com>
2024-02-19 19:02:08 +0200

    walmart

--- Frontend/Walmart/src/components/Card.jsx
+++ Frontend/Walmart/src/components/Card.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Card = ({ photo, title, price }) => {
     return (
-        <div className="flex flex-col items-center justify-center ">
+        <div className="relative flex flex-col w-100 h-100">
             <img src={photo} alt="" className="w-52 h-56 rounded-[1rem]" />
-            <div className="flex justify-between w-[13rem]">
-                <p className="font-bold text-[13px] ">
-                    {title + "Rapid Charger & Stylus Pen  "}
-                </p>
+            <div className="flex flex-col w-[13rem]">
                 <p className="font-bold">{price}</p>
+                <p className="font-bold text-[13px] ">
+                    {title + "Rapid Charger & Stylus Pen"}
+                </p>
             </div>
         </div>
     );
