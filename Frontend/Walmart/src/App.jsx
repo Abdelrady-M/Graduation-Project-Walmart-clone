a95dd65e52ebfafcf201d5ed0e755f441b377a1b
DevAbdelrady <ahmedmc95@gmail.com>
2024-02-20 19:20:07 +0200

    rady

--- Frontend/Walmart/src/App.jsx
+++ Frontend/Walmart/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import AppLayout from "./AppLayout";
-import Electronics from "./pages/Electronics";
+// import Electronics from "./pages/Electronics";
 import Login from "./pages/login"
 import Register from "./pages/register"
 
@@ -10,10 +10,7 @@ const routes = createBrowserRouter([
     path: "/",
     element: <AppLayout />,
   },
-  {
-    path: "/electronics",
-    element: <Electronics />,
-  },
+
   {
     path: "/login",
     element: <Login />,
--- Frontend/Walmart/src/components/Deals.jsx
+++ Frontend/Walmart/src/components/Deals.jsx
@@ -7,68 +7,120 @@ import Card from "./Card";
 import Slider from "react-slick";
 import { MdOutlineArrowForwardIos, MdOutlineArrowBackIos } from "react-icons/md";
 
+
+const cards = [
+    { photo: costume, title: "Costume", price: "$17" },
+    { photo: jacket, title: "Costume", price: "$17" },
+    { photo: jacket2, title: "Costume", price: "$17" },
+    { photo: jacket3, title: "Costume", price: "$17" },
+    { photo: costume, title: "Costume", price: "$17" },
+    { photo: jacket, title: "Costume", price: "$17" },
+    { photo: jacket2, title: "Costume", price: "$17" },
+    { photo: jacket3, title: "Costume", price: "$17" },
+    { photo: costume, title: "Costume", price: "$17" },
+    { photo: jacket, title: "Costume", price: "$17" },
+    { photo: jacket2, title: "Costume", price: "$17" },
+    { photo: jacket3, title: "Costume", price: "$17" },
+    { photo: costume, title: "Costume", price: "$17" },
+    { photo: jacket, title: "Costume", price: "$17" },
+    { photo: jacket2, title: "Costume", price: "$17" },
+];
+
+
+function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+        <div
+            className={className}
+            style={{ ...style, display: "block", background: "red" }}
+            onClick={onClick}
+        >
+            NEXT
+        </div>
+    );
+}
+
+function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+        <div
+            className={className}
+            style={{ ...style, display: "block", background: "green" }}
+            onClick={onClick}
+        >
+            BACK
+        </div>
+    );
+}
 const Deals = () => {
+    const slider = React.useRef(null);
+
     const settings = {
-        className: "center",
-        centerMode: true,
+        dots: true,
         infinite: true,
-        centerPadding: "5px",
-        slidesToShow: 6,
-        speed: 200,
-        prevArrow: <CustomPrevArrow />,
-        nextArrow: <CustomNextArrow />
+        arrows: false,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+
+        responsive: [
+            {
+                breakpoint: 1424,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true,
+                },
+            },
+
+            {
+                breakpoint: 1124,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true,
+                    dots: true,
+                },
+            },
+            {
+                breakpoint: 800,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    initialSlide: 2,
+                },
+            },
+        ],
+
+        nextArrow: <SampleNextArrow />,
+        prevArrow: <SamplePrevArrow />,
     };
 
-    const cards = [
-        { photo: costume, title: "Costume", price: "$17" },
-        { photo: jacket, title: "Costume", price: "$17" },
-        { photo: jacket2, title: "Costume", price: "$17" },
-        { photo: jacket3, title: "Costume", price: "$17" },
-        { photo: costume, title: "Costume", price: "$17" },
-        { photo: jacket, title: "Costume", price: "$17" },
-        { photo: jacket2, title: "Costume", price: "$17" },
-        { photo: jacket3, title: "Costume", price: "$17" },
-        { photo: costume, title: "Costume", price: "$17" },
-        { photo: jacket, title: "Costume", price: "$17" },
-        { photo: jacket2, title: "Costume", price: "$17" },
-        { photo: jacket3, title: "Costume", price: "$17" },
-        { photo: costume, title: "Costume", price: "$17" },
-        { photo: jacket, title: "Costume", price: "$17" },
-        { photo: jacket2, title: "Costume", price: "$17" },
-    ];
-
     return (
-        <div className="container mx-auto mt-10 flex flex-col w-full relative">
-            <div className="mb-5 font-bold"><h1>Flash Deals</h1></div>
-            <div className="md:w-max xl:w-[80vw] mt-4 mx-auto ">
-                <Slider {...settings}>
-                    {cards.map((card, index) => (
-                        <div key={index}>
-                            <Card photo={card.photo} title={card.title} price={card.price} />
-                        </div>
-                    ))}
-                </Slider>
+        <div className="container mb-10 mx-auto">
+            <h1 style={{ textAlign: "center", margin: "20px 0px" }}>
+                DEALS
+            </h1>
+
+            <div style={{ margin: 20 }}>
+                <button onClick={() => slider?.current?.slickPrev()}>Prev</button>
+                <button
+                    style={{ marginLeft: 20 }}
+                    onClick={() => slider?.current?.slickNext()}
+                >
+                    Next
+                </button>
             </div>
+
+            <Slider ref={slider} {...settings}>
+                {cards.map((card, index) => (
+                    <div key={index}>
+                        <Card photo={card.photo} title={card.title} price={card.price} />
+                    </div>
+                ))}
+            </Slider>
         </div>
     );
-};
-
-const CustomPrevArrow = (props) => {
-    const { onClick } = props;
-    return (
-        <div className="slick-arrow custom-prev-arrow absolute top-0 left-0 z-40 w-[40px] h-[40px] rounded-full border-black bg-inherit" onClick={onClick}>
-            <MdOutlineArrowBackIos />
-        </div>
-    );
-};
-
-const CustomNextArrow = (props) => {
-    const { onClick } = props;
-    return (
-        <div className="slick-arrow custom-next-arrow absolute top-0 right-0 z-40" onClick={onClick}>
-            <MdOutlineArrowForwardIos />
-        </div>
-    );
-};
-
-export default Deals;
+}
+export default Deals
