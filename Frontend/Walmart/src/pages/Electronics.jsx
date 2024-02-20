55e95d58dc272e7c2c9ec9d1f178ef955d41448d
aya-m-98 <106470807+aya-m-98@users.noreply.github.com>
2024-02-20 19:53:45 +0200

    'update'

--- Frontend/Walmart/src/pages/Electronics.jsx
+++ Frontend/Walmart/src/pages/Electronics.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import MultiCarousal from "../components/MultiCarousal";
+import LeftHandNavList from "../components/LeftHandNavList";
 
 export default function Electronics() {
   return (
@@ -13,6 +14,12 @@ export default function Electronics() {
             title="Apple Deals"
             desc="Save on the gadgets you love, for less."
           />
+        </section>
+        <section className="flex justify-between">
+          <aside className="h-100 relative w-25 flex -column">
+            <LeftHandNavList />
+          </aside>
+          <article></article>
         </section>
       </main>
       <Footer />
