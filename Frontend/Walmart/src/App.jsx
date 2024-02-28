255518ac658202d71389b5b563d9178edd0b41c6
DevAbdelrady <ahmedmc95@gmail.com>
2024-02-28 15:22:33 +0200

    updated last

--- Frontend/Walmart/src/App.jsx
+++ Frontend/Walmart/src/App.jsx
@@ -2,19 +2,13 @@ import React from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import AppLayout from "./AppLayout";
 import Electronics from "./pages/Electronics";
-import Register from "./pages/register";
-// import "bootstrap/dist/css/bootstrap.min.css";
 import Grocery from "./pages/Grocery.jsx";
 import Home2 from "./pages/Home2.jsx";
 import Cart from "./pages/Cart/Cart.jsx";
 import Checkout from "./pages/Checkout/Checkout.jsx";
 import Fashion from "./pages/Fashion.jsx";
-import "bootstrap/dist/css/bootstrap.min.css";
-//import GroceriesEessentials from "./pages/GroceriesEssentials.jsx";
-//import DetailsPage from "./pages/details/Details.jsx";
 import Details from "./pages/details/Details.jsx";
 import Home from "./pages/HomePage/Home.jsx";
-import Login from './pages/login/login.jsx';
 
 
 const routes = createBrowserRouter([
@@ -54,14 +48,7 @@ const routes = createBrowserRouter([
     ],
   },
 
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/register",
-    element: <Register />,
-  },
+
 ]);
 const App = () => {
   return <RouterProvider router={routes} />;
--- Frontend/Walmart/src/pages/login/login.jsx
+++ Frontend/Walmart/src/pages/login/login.jsx
@@ -1,121 +1,95 @@
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
+// import Container from 'react-bootstrap/Container';
+// import Row from 'react-bootstrap/Row';
 
 
-import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import { EyeInvisibleOutlined, EyeOutlined } from "@ant-design/icons";
-import { useForm } from 'react-hook-form';
-import './login.css'; // Import the CSS file
+// import React, { useState } from 'react';
+// import Button from 'react-bootstrap/Button';
+// import Form from 'react-bootstrap/Form';
+// import { EyeInvisibleOutlined, EyeOutlined } from "@ant-design/icons";
+// import { useForm } from 'react-hook-form';
+// import './login.css'; // Import the CSS file
 
-export const Login = () => {
-    const { register, handleSubmit, formState: { errors }, watch } = useForm({ mode: 'onChange' });
-    const [visible, setVisible] = useState(false);
+// export const Login = () => {
+//     const { register, handleSubmit, formState: { errors }, watch } = useForm({ mode: 'onChange' });
+//     const [visible, setVisible] = useState(false);
 
-    // to handle api
-    const onSubmit = data => console.log(data);
+//     // to handle api
+//     const onSubmit = data => console.log(data);
 
-    return (<>
-        <Container className='align-center'>
-            <Row className='col-md-5 mx-auto'>
-                <header className="h d-b ">
-                    <a aria-atomic="true" aria-label="Walmart. Save Money. Live Better. Home Page" href="/">
-                        <img loading="lazy" src="https://i5.walmartimages.com/dfw/4ff9c6c9-d10e/k2-_ef2c8660-96ed-4f64-891d-329fa488c482.v1.png" alt="Walmart logo with link to homepage."
-                            height="65" width="64" />
-                    </a>
-                    <h1 className='h'>Sign in or create your account</h1>
-                    <p className="mb4 m0 fontp">Not sure if you have an account?
-                        <br /> Enter your email and we’ll check for you.
-                    </p>
-                </header>
-
-
-                <Form className='mt-1 mp-1'>
-
-                    <Form.Group className="mb-3" >
-
-                        <Form.Label className='f'>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="name@example.com" as="textarea" rows={3} />
-                    </Form.Group>
-                    <button className='buttonSub' type="submit" id="login-continue-button" >Continue</button>
-                    <p className="para">Securing your personal information is our priority.
-                        <br /><a className="dark-gray a" target="_blank" href="https://corporate.walmart.com/privacy-security">See our privacy measures.
-                        </a></p>
-                </Form>
-
-            </Row>
-        </Container>
-
-        <footer  className="foot" style={{ maxWidth: '1280px' }}>
-            <div className=" ml-0 mt-4 pb-5 pt-1  flex-shrink-0">© 2024 Walmart. All Rights Reserved.</div>
-            <ul className="list mt-4 mb-0" style={{ flexBasis: '100%' }}>
-                <li className=" D ml-4">
-                    <button className=" footbtn bg-transparent mb-3 ml-3 ml-4-l" type="button" style={{ color: 'rgb(70, 71, 74)', marginLeft: '0px' }}>Give feedback</button>
-                </li>
-                <li className=" D ml-4">
-                    <a className="no-underline tag mb-3 flex items-center" target="_blank" href="https://corporate.walmart.com/privacy-security/california-privacy-rights">
-                        <span>CA Privacy Rights</span>
-                    </a>
-                </li>
-                <li className="D ml-4">
-                    <a className=" no-underline tag mb-3 flex items-center" target="_blank" href="https://www.walmart.com/account/api/ccpa-intake?native=false&amp;app=gm&amp;type=sod">
-                        <img loading="lazy" width="29" height="14" src="//i5.walmartimages.com/dfwrs/76316474-d730/k2-_3c5ba298-4f19-46be-9fc3-ac49225d19bd.v1.png" className="mr-1" />
-                        <span className="self-baseline">Your Privacy Choices</span>
-                    </a>
-                </li>
-                <li className="D ml-4">
-                    <a className="no-underline mb-3 flex items-center" target="_blank" href="https://corporate.walmart.com/privacy-security/walmart-privacy-notice#what-type-of-information-do-we-collect-and-how-do-we-collect-it">
-                        <span className="">Notice at Collection</span>
-                    
-                    </a>
-                </li>
-                <li className=" D ml-4">
-                    <a className="no-underline mb-3 flex items-center"  target="_blank" href="https://www.walmart.com/account/api/ccpa-intake?native=false&amp;app=gm&amp;type=access">
-                        <span>Request My Personal Information</span>
-                        
-                    </a>
-                </li>
-                <li className="D ml-4">
-                    <a className="no-underline mb-3 flex items-center" target="_blank" href="https://corporate.walmart.com/california-transparency">
-                        <span className="">California Supply Chains Act</span>
-                    </a>
-                </li>
-            </ul>
-        </footer>
+//     return (<>
+//         <Container className='align-center'>
+//             <Row className='col-md-5 mx-auto'>
+//                 <header className="h d-b ">
+//                     <a aria-atomic="true" aria-label="Walmart. Save Money. Live Better. Home Page" href="/">
+//                         <img loading="lazy" src="https://i5.walmartimages.com/dfw/4ff9c6c9-d10e/k2-_ef2c8660-96ed-4f64-891d-329fa488c482.v1.png" alt="Walmart logo with link to homepage."
+//                             height="65" width="64" />
+//                     </a>
+//                     <h1 className='h'>Sign in or create your account</h1>
+//                     <p className="mb4 m0 fontp">Not sure if you have an account?
+//                         <br /> Enter your email and we’ll check for you.
+//                     </p>
+//                 </header>
 
 
+//                 <Form className='mt-1 mp-1'>
 
+//                     <Form.Group className="mb-3" >
+
+//                         <Form.Label className='f'>Email address</Form.Label>
+//                         <Form.Control type="email" placeholder="name@example.com" as="textarea" rows={3} />
+//                     </Form.Group>
+//                     <button className='buttonSub' type="submit" id="login-continue-button" >Continue</button>
+//                     <p className="para">Securing your personal information is our priority.
+//                         <br /><a className="dark-gray a" target="_blank" href="https://corporate.walmart.com/privacy-security">See our privacy measures.
+//                         </a></p>
+//                 </Form>
+
+//             </Row>
+//         </Container>
+
+//         <footer className="foot" style={{ maxWidth: '1280px' }}>
+//             <div className=" ml-0 mt-4 pb-5 pt-1  flex-shrink-0">© 2024 Walmart. All Rights Reserved.</div>
+//             <ul className="list mt-4 mb-0" style={{ flexBasis: '100%' }}>
+//                 <li className=" D ml-4">
+//                     <button className=" footbtn bg-transparent mb-3 ml-3 ml-4-l" type="button" style={{ color: 'rgb(70, 71, 74)', marginLeft: '0px' }}>Give feedback</button>
+//                 </li>
+//                 <li className=" D ml-4">
+//                     <a className="no-underline tag mb-3 flex items-center" target="_blank" href="https://corporate.walmart.com/privacy-security/california-privacy-rights">
+//                         <span>CA Privacy Rights</span>
+//                     </a>
+//                 </li>
+//                 <li className="D ml-4">
+//                     <a className=" no-underline tag mb-3 flex items-center" target="_blank" href="https://www.walmart.com/account/api/ccpa-intake?native=false&amp;app=gm&amp;type=sod">
+//                         <img loading="lazy" width="29" height="14" src="//i5.walmartimages.com/dfwrs/76316474-d730/k2-_3c5ba298-4f19-46be-9fc3-ac49225d19bd.v1.png" className="mr-1" />
+//                         <span className="self-baseline">Your Privacy Choices</span>
+//                     </a>
+//                 </li>
+//                 <li className="D ml-4">
+//                     <a className="no-underline mb-3 flex items-center" target="_blank" href="https://corporate.walmart.com/privacy-security/walmart-privacy-notice#what-type-of-information-do-we-collect-and-how-do-we-collect-it">
+//                         <span className="">Notice at Collection</span>
+
+//                     </a>
+//                 </li>
+//                 <li className=" D ml-4">
+//                     <a className="no-underline mb-3 flex items-center" target="_blank" href="https://www.walmart.com/account/api/ccpa-intake?native=false&amp;app=gm&amp;type=access">
+//                         <span>Request My Personal Information</span>
+
+//                     </a>
+//                 </li>
+//                 <li className="D ml-4">
+//                     <a className="no-underline mb-3 flex items-center" target="_blank" href="https://corporate.walmart.com/california-transparency">
+//                         <span className="">California Supply Chains Act</span>
+//                     </a>
+//                 </li>
+//             </ul>
+//         </footer>
 
 
 
 
 
-    </>
-        // <div classNameName="login-container">
-        //     <Form classNameName="login-form" onSubmit={handleSubmit(onSubmit)}>
-        //         <Form.Group classNameName="login-input" controlId="formBasicEmail">
-        //             <Form.Label>Email address</Form.Label>
-        //             <Form.Control type="email" placeholder="Enter email"
-        //                 {...register("email", { required: true, pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/ })} />
-        //             {errors.email && <p classNameName="text-danger">Invalid Email</p>}
-        //         </Form.Group>
 
-        //         <Form.Group classNameName="login-input" controlId="formBasicPassword">
-        //             <Form.Label>Password</Form.Label>
 
-        //                 <Form.Control type={visible ? "text" : "password"} placeholder="Password"
-        //                     {...register('password', { required: true, minLength: 8 })} />
-        //                 <div classNameName='p-2' onClick={() => setVisible(!visible)}>
-        //                     {visible ? <EyeOutlined /> : <EyeInvisibleOutlined />}
-        //             </div>
-        //             {errors.password && <p classNameName="text-danger">Invalid Password</p>}
-        //         </Form.Group>
 
-        //         <Button as="input" type="submit" value="Submit" />
-        //     </Form>
-        // </div>
-    );
-}
 
-export default Login;
+// export default Login;
