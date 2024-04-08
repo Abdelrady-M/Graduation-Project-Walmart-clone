fdba6750521e8635257fe9df08421134f5b1ad59
aya-m-98 <106470807+aya-m-98@users.noreply.github.com>
2024-04-08 06:36:15 +0200

    address

--- Frontend/Walmart/src/store/store.js
+++ Frontend/Walmart/src/store/store.js
@@ -1,29 +1,30 @@
 import { configureStore } from "@reduxjs/toolkit";
-import productReducer from "./slices/product"
-import authReducer from './slices/authSlice'
-import logReducer from './slices/authLogin'
-import subCategoriesReducer from "./slices/subcategory"
-import categoryReducer from "./slices/categories"
-import cartReducer from "./slices/cart"
-import wishListReducer from "./slices/wishList"
+import productReducer from "./slices/product";
+import authReducer from "./slices/authSlice";
+import logReducer from "./slices/authLogin";
+import subCategoriesReducer from "./slices/subcategory";
+import categoryReducer from "./slices/categories";
+import cartReducer from "./slices/cart";
+import wishListReducer from "./slices/wishList";
 import oneOrderSlice from "./slices/order";
-import checkOutReducer from "./slices/checkOut"
-import userReducer from './slices/user'
+import checkOutReducer from "./slices/checkOut";
+import userReducer from "./slices/user";
+import addressesSlice from "./slices/addresses";
 
 const store = configureStore({
-    reducer: {
-        products: productReducer,
-        auth: authReducer,
-        login: logReducer,
-        subCategories: subCategoriesReducer,
-        categories: categoryReducer,
-        cart: cartReducer,
-        wishList: wishListReducer,
-        oneOrder: oneOrderSlice,
-        checkOut: checkOutReducer,
-        user: userReducer,
-
-    },
+  reducer: {
+    products: productReducer,
+    auth: authReducer,
+    login: logReducer,
+    subCategories: subCategoriesReducer,
+    categories: categoryReducer,
+    cart: cartReducer,
+    wishList: wishListReducer,
+    oneOrder: oneOrderSlice,
+    checkOut: checkOutReducer,
+    user: userReducer,
+    addresses: addressesSlice,
+  },
 });
 
 export default store;
